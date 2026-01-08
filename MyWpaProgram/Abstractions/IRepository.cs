66296bb9d5e76ea996b2856bcb6eb651ec549e9c
James Bright <james@bright.me.uk>
2026-01-08 18:47:06 +0000

    Irepository comments

--- MyWpaProgram/Abstractions/IRepository.cs
+++ MyWpaProgram/Abstractions/IRepository.cs
@@ -5,5 +5,16 @@ public interface IRepository<T>
     Task<IReadOnlyList<T>> GetAllASync(CancellationToken ct = default);
 }
 
-//IRepository<T> = generic interface - T could be anything
-//Task is an async method
+/*
+
+IRepository<T> = generic interface - T could be anything
+
+The type for this interface method:
+      Task = async method: Task<TYPE>
+      IReadOnlyList = Read only list: IReadOnlyList<TYPE>
+
+The argument of this method:
+    CancellationToken = .NET way of letting an async method cancel. Not a kill switch, but allows it to stop ealry
+    default = default value, so for ct its dont cancel/not cancellable
+
+*/
