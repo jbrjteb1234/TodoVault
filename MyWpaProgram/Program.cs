885517dbfa7c5bdb707e31068ca58e4be28608d7
James Bright <james@bright.me.uk>
2026-01-25 14:44:44 +0000

    Implemented initial entry point for program

--- MyWpaProgram/Program.cs
+++ MyWpaProgram/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyWpaProgram.Data;
+using MyWpaProgram.Exceptions;
+using MyWpaProgram.Services;
+
+namespace MyWpaProgram;
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        try
+        {
+            //build path from the base directory to sample-todos.json
+            var JsonPath = Path.Combine(AppContext.BaseDirectory, "Data", "sample-todos.json");
+        }
+        catch(AppException ex)
+        {
+            Console.WriteLine("Application Exception: "+ex.Message+"\n\n"+"Information: "+ex.InnerException);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Unexpected error: "+ex.Message);
+        }
+    }
+}
