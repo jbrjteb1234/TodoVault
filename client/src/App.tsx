2e19dfe661695e937aa986f18793296ef966a645
James Bright <james@bright.me.uk>
2026-01-30 16:59:37 +0000

    Initial commit for react-ts frontend through vite

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+//Default app to export
+export default function App(){
+    return (
+        <main style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+            <h1>TodoVault</h1>
+            <p>Frontend scaffold is running.</p>
+        </main>
+    )
+}
