3a194dc3c6dea274e437300265f24e89fe88d740
James Bright <james@bright.me.uk>
2026-02-01 20:57:44 +0000

    Sample list of todos using todo file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
+//import a TYPE from the file
+import type { Todo } from "./types/todo.ts";
 
 const layoutSize = {
     padding: 24,
     fontFamily: "system-ui, sans-serif"
 }
+
+//array of the type Todo
+const sampleTodos: Todo[] = [
+    { id: 1, title: "Wire up frontend scaffold", isDone: true },
+    { id: 2, title: "Create Todo list UI", isDone: false },
+    { id: 3, title: "Connect to API", isDone: false },
+];
 
 //Default app to export
 export default function App(){
