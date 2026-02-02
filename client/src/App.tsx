80bdb1fee1e04c4b2b32b88d39b6f7e718c4fa48
James Bright <james@bright.me.uk>
2026-02-02 18:17:05 +0000

    Initial useState and useEffect for error, todoList and loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,45 @@
 //import a TYPE from the file
 import type { Todo } from "./types/todo.ts";
+import { useEffect, useState} from "react";
+import { getTodos } from "./api/TodoApis.ts";
 
 const layoutSize = {
     padding: 24,
     fontFamily: "system-ui, sans-serif"
 }
 
-//array of the type Todo
-const sampleTodos: Todo[] = [
-    { id: 1, title: "Wire up frontend scaffold", isDone: true },
-    { id: 2, title: "Create Todo list UI", isDone: false },
-    { id: 3, title: "Connect to API", isDone: false },
-];
-
 //Default app to export
 export default function App(){
+    //State: array of toods loaded onto the frontend
+    const [todos, setTodos] = useState<Todo[]>( new Array<Todo>() ); //UseState for arrays loaded on the frontend, init to empty array of Todos
+
+    //States: loading a Todo and an error state
+    const [loading, setLoading] = useState<boolean>( false );
+    const [error, setError] = useState<string | null>(null);    //type is a union of string and null
+
+    //Fetch the TODOs when the component first loads
+    useEffect(() => {
+        async function loadTodos(){
+            try{
+                setLoading(true);
+                setError(null);
+
+                const data:Todo[] = await getTodos();
+                setTodos(data);
+            }catch{
+                setError("Failed to fetch Todos.")
+            }finally{
+                setLoading(false);
+            }
+        }
+
+        void loadTodos();
+    }, []); //dependancy array - [] means run once when the component first renders
+    
     return (
         <main style={layoutSize}>
             <h1>TodoVault</h1>
-            <p>Frontend scaffold is running.</p>
         </main>
     )
+
 }
