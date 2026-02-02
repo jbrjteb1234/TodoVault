db4612b6be1b2bbe5030d45c6efb9c106df2dcc9
James Bright <james@bright.me.uk>
2026-02-02 18:28:34 +0000

    Matched up config on backend and vite.config for testing

--- TodoVault/Program.cs
+++ TodoVault/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("frontend", policy =>
-        policy.WithOrigins("http://localhost:5500", "http://localhost:5173")
+        policy.WithOrigins("http://localhost:5000")
               .AllowAnyHeader()
               .AllowAnyMethod()
     );
