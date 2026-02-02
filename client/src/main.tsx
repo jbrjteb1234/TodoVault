eceacb0db0fdbc1a14db25451f472023ba0b7b7e
James Bright <james@bright.me.uk>
2026-02-02 18:29:04 +0000

    Imlemented html to include todo data fetched from backend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
-import App from './App.tsx'
+import App from './App.tsx' //Unnamed import (no curly braces) so we are importing the default export from app.tsx
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
