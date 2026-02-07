06dc5e179d9292027ac0a123faac4a853ff35738
James Bright <james@bright.me.uk>
2026-02-07 01:04:36 +0000

    Todo type on frontend matches backend tpye

--- client/src/types/todo.ts
+++ client/src/types/todo.ts
@@ -4,4 +4,9 @@ export type Todo = {
     id: number;
     title: string;
     isDone: boolean;
+    priority: number;
+    owner: string;
+    category: string;
+    dueDate?: string | null;
+    notes?: string | null;
 };
