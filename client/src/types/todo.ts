746d71455538faa12cc5d6d8465c055adb21b9f9
James Bright <james@bright.me.uk>
2026-02-07 01:18:26 +0000

    Implemented DTOs for todo.ts

--- client/src/types/todo.ts
+++ client/src/types/todo.ts
@@ -1,12 +1,21 @@
 //for type files, use export type. export makes it exportable anywhere
 
-export type Todo = {
-    id: number;
+type TodoCore = {
     title: string;
-    isDone: boolean;
     priority: number;
     owner: string;
     category: string;
     dueDate?: string | null;
     notes?: string | null;
+};
+
+export type Todo =  TodoCore & {
+    id: number;
+    isDone: boolean;
+};
+
+export type CreateTodoDto = TodoCore;
+
+export type UpdateTodoDto = TodoCore & {
+    isDone: boolean;
 };
