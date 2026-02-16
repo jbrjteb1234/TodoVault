d6221a32069cb1a866ceb34d1d9c4191f30b0d8e
James Bright <james@bright.me.uk>
2026-02-16 20:08:09 +0000

    Implemented description list under each display todo

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -8,7 +8,7 @@ type CreateTodoDisplayProps = {
 //This function is a react component, so the argument must be a prop object
 export default function createTodoDisplay({todo}: CreateTodoDisplayProps){
 
-    const [expanded, setExpanded] = useState<boolean>(false);
+    const [expanded, setExpanded] = useState<boolean>(true);
 
     return (
         <li key={todo.id} style={{ marginBottom: 8 }}>
@@ -17,14 +17,17 @@ export default function createTodoDisplay({todo}: CreateTodoDisplayProps){
                 <span style={{ textDecoration: todo.isDone ? "line-through" : "none" }}>
                     {todo.title}
                 </span>
-
-                <br></br>
-
-                <ul>
-                    
-                </ul>
-
             </label>
+            
+            {expanded && (
+                <ul>
+                    <li>Priority: {todo.priority}</li>
+                    <li>Owner: {todo.owner}</li>
+                    <li>Category: {todo.category}</li>
+                    {todo.dueDate && (<li>Due date: {todo.dueDate}</li>)}
+                    {todo.notes && (<li>Notes: {todo.notes}</li>)}
+                </ul>
+            )}
         </li>
     );
 }
