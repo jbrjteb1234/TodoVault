c377583812e0a5d6ee30ed7f57a8a1f2807abd36
James Bright <james@bright.me.uk>
2026-02-23 22:36:44 +0000

    Changed todo type definitions for more accurate DTOs

--- client/src/types/todo.ts
+++ client/src/types/todo.ts
@@ -1,11 +1,10 @@
 //for type files, use export type. export makes it exportable anywhere
 
 export const PriorityOptions = [1,2,3,4,5] as const;
-export type Priority = (typeof PriorityOptions)[number] | null;
+export type Priority = (typeof PriorityOptions)[number];
 
 export type TodoCore = {
     title: string;
-    priority: Priority;
     owner: string;
     description: string;
     dueDate?: string | null;
@@ -13,12 +12,16 @@ export type TodoCore = {
 };
 
 export type Todo =  TodoCore & {
+    priority: Priority;
     id: number;
     isDone: boolean;
 };
 
-export type CreateTodoDto = TodoCore;
+export type CreateTodoDto = TodoCore & {
+    priority: Priority | null;
+};
 
 export type UpdateTodoDto = TodoCore & {
+    priority: Priority | null;
     isDone: boolean;
 };
